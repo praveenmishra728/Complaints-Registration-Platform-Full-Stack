1306403a30ebdc275a286d053b6a33f83342bc99
praveen mishra <pravennmishra728@gmail.com>
2026-05-22 21:57:08 +0530

    change 22.5.26

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,4 +1,4 @@
-const BACKEND_BASE_URL = 'http://127.0.0.1:3000';
+const BACKEND_BASE_URL = 'https://complaints-registration-platform-full-o0bp.onrender.com';
 const API_BASE = `${BACKEND_BASE_URL}/api`;
 // DOM Elements
 const sections = {
